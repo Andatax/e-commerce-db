cca525b3e3b6c2b71594ba9ddc2422d8a6be4894
Andatax <abraham.mendez1011@gmail.com>
2024-01-16 16:18:18 -0600

    Models done

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+ProductTag.init({
+	// define columns
+	id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	product_id: {
+		type: DataTypes.INTEGER,
+		references: {
+			model: Product,
+			key: "id",
+		},
+	},
+	tag_id: {
+		type: DataTypes.INTEGER,
+		references: {
+			model: Tag,
+			key: "id",
+		},
+	},
+});
 
 module.exports = ProductTag;
